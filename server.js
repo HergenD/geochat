99be2dff5c4a4bfe17be05cba7c99ec1a9e7171a
Hergen Dillema <hergen.dillema@gmail.com>
2021-02-24 18:37:17 +0100

    r e f a c t o r * v 1

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+// Requires
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 var async = require('async');
+
+// Globals
 const port = process.env.PORT || 3000;
 let users = {};
 let rooms = {};
 
+// Web server
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/create.html');
 });
@@ -13,24 +17,64 @@ app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/play.html');
 });
 
-function stats(room){
+// Websocket connection
+io.on('connection', (socket) => {
+    let connection = createGlobalConnection(socket);
+
+    socket.on('user connect', userId => connectUser(connection, userId));
+    socket.on('create room', options => createRoom(connection, options));
+    socket.on('start game', roomId => startGame(connection, roomId));
+    socket.on('join room', roomId => joinRoom(connection, roomId));
+    socket.on('submit guess', guess => processGuess(connection, guess));
+    socket.on("disconnect", () => disconnectUser(connection));
+});
+
+
+http.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+function checkIfGuessAllowed(room, userId) {
+    const currentRound = room.currentRound;
+    if (!room.gameRunning || room.players[userId][currentRound]) {
+        return false;
+    }
+    return true;
+}
+
+function submitGuess(room, user, guess) {
+    const currentRound = room.currentRound;
+    room.guesses[currentRound].push({ lat: guess.lat, lon: guess.lon })
+    room.players[user][currentRound] = true;
+}
+
+function runRoomInfoLoop(roomId) {
     async.whilst(
-        function (callbackFunction) {
-            let counter = 0;
-            // start loop
-            // loopStartTime = Date.now();
-            callbackFunction(null, counter >= 0);
+        function (cb) {
+            cb(null, true);
         },
         function (callback) {
-            io.to(room).emit("stats", {
-                players: rooms[room].playerCount
+            io.to(roomId).emit("stats", {
+                players: rooms[roomId].playerCount
             })
             setTimeout(callback, 1000);
         }
     )
 }
 
-function gameLoop(room, duration, round) {
+function startGameLoop(room) {
+    let round = 1;
+    const duration = rooms[room].duration;
     io.to(room).emit("round", {
         round,
         lat: 51.505,
@@ -46,8 +90,6 @@ function gameLoop(room, duration, round) {
         function (callback) {
             //logic
             io.to(room).emit("countdown", rooms[room].currentSeconds);
-
-            // end loop
             waitTime = 1000;
             if (rooms[room].currentSeconds === 0) {
                 if (round < rooms[room].rounds) {
@@ -62,11 +104,11 @@ function gameLoop(room, duration, round) {
                     }
                     rooms[room].lat = totalLat / rooms[room].guesses[round].length;
                     rooms[room].lon = totalLon / rooms[room].guesses[round].length;
-                    rooms[room].status = 0;
-                    
-                    for(let i =0; i< Object.keys(rooms[room].players).length;i++){
+                    rooms[room].gameRunning = false;
+
+                    for (let i = 0; i < Object.keys(rooms[room].players).length; i++) {
                         rooms[room].players[Object.keys(rooms[room].players)[i]] = {};
-                    } 
+                    }
                     io.to(room).emit("finished", {
                         round,
                         lat: rooms[room].lat,
@@ -81,7 +123,7 @@ function gameLoop(room, duration, round) {
 
                 let totalLat = 0;
                 let totalLon = 0;
-                if(rooms[room].guesses[round].length){
+                if (rooms[room].guesses[round].length) {
                     for (let i = 0; i < rooms[room].guesses[round].length; i++) {
                         totalLat += rooms[room].guesses[round][i].lat
                         totalLon += rooms[room].guesses[round][i].lon
@@ -110,116 +152,129 @@ function gameLoop(room, duration, round) {
     )
 }
 
-io.on('connection', (socket) => {
-    users[socket.id] = {}
+function createGlobalConnection(socket) {
+    users[socket.id] = { socket }
+    return users[socket.id];
+}
 
-    socket.on('user connect', id => {
-        if (id === "create") {
-            id = makeid(24);
-        }
-        users[socket.id].userId = id;
-        socket.emit("user connected", users[socket.id].userId);
-    });
-
-    socket.on('create room', options => {
-        let room = makeid(5);
-        while (rooms[room]) {
-            room = makeid(5);
-        }
-        const secret = makeid(24);
-        const rounds = parseInt(options.rounds);
-        const duration = parseInt(options.duration);
-        const roomName = options.roomName;
-        rooms[room] = {
-            room,
-            status: 0,
-            admin: users[socket.id].userId,
-            secret,
-            rounds,
-            currentRound: 0,
-            currentSeconds: duration,
-            guesses: {},
-            zooms: options.zooms,
-            duration,
-            players: {},
-            lat: 51.505,
-            lon: -0.09,
-            playerCount: 0,
-            roomName
-        };
-
-        socket.emit("room created", rooms[room]);
-        stats(room);
-    });
-
-    socket.on('start game', room => {
-        if (users[socket.id].userId !== rooms[room].admin || rooms[room].status) {
-            return
-        }
-        rooms[room].status = 1;
-        socket.emit("game started", true)
-        gameLoop(room, rooms[room].duration, 1);
-    });
-
-    socket.on('join room', room => {
-        if (rooms[room] == undefined) {
-            socket.emit("redirect", "/")
-            return
-        }
-        if(rooms[room].players[users[socket.id].userId] == undefined){
-            rooms[room].players[users[socket.id].userId] = {}
-        }
-
-        rooms[room]
-        users[socket.id].room = room;
-        socket.join(room);
-        let canGuess = false;
-        if(rooms[room].status && !rooms[room].players[users[socket.id].userId][rooms[room].currentRound]){
-            canGuess = true;
-        }
-        socket.emit("room joined", { 
-            room, 
-            rounds: rooms[room].rounds, 
-            duration: rooms[room].duration, 
-            roomName: rooms[room].roomName ?? "WHOMEGALUL", 
-            admin: (users[socket.id].userId === rooms[room].admin),
-            status: rooms[room].status,
-            currentRound: rooms[room].currentRound,
-            currentSeconds: rooms[room].currentSeconds,
-            canGuess
-         })
-         rooms[room].playerCount++;
-    });
-
-    socket.on('submit guess', guess => {
-
-        if(!rooms[guess.room].status || rooms[guess.room].players[users[socket.id].userId][rooms[guess.room].currentRound]){
-            return;
-        }
-
-        rooms[guess.room].guesses[rooms[guess.room].currentRound].push({ lat: guess.lat, lon: guess.lon })
-        rooms[guess.room].players[users[socket.id].userId][rooms[guess.room].currentRound] = true;
-    });
-
-    socket.on("disconnect", (reason) => {
-        if(users[socket.id] && users[socket.id].room){
-            rooms[users[socket.id].room].playerCount--;
-        }
-        delete users[socket.id];
-    });
-
-});
-
-http.listen(port, () => {
-    console.log(`Socket.IO server running at http://localhost:${port}/`);
-});
-
-function makeid(length) {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+function assignUserId(connection, id) {
+    if (id === "create") {
+        id = makeid(24);
     }
-    return result;
+    connection.userId = id;
+}
+
+function makeRoomId() {
+    let room = makeid(5);
+    while (rooms[room]) {
+        room = makeid(5);
+    }
+    return room;
+}
+
+function createRoom(connection, options) {
+
+    let roomId = makeRoomId();
+    const rounds = parseInt(options.rounds);
+    const duration = parseInt(options.duration);
+    const roomName = options.roomName;
+
+    rooms[roomId] = {
+        room: roomId, // roomId refactor
+        gameRunning: false,
+        admin: connection.userId,
+        rounds,
+        currentRound: 0,
+        currentSeconds: duration,
+        guesses: {},
+        zooms: options.zooms,
+        duration,
+        players: {},
+        lat: 51.505,
+        lon: -0.09,
+        playerCount: 0,
+        roomName
+    };
+
+    connection.socket.emit("room created", rooms[roomId]);
+
+    runRoomInfoLoop(roomId);
+}
+
+function connectUser(connection, id) {
+    assignUserId(connection, id);
+
+    connection.socket.emit("user connected", connection.userId);
+}
+
+function startGame(connection, roomId) {
+    const room = rooms[roomId];
+    if (connection.userId !== room.admin || room.gameRunning) {
+        return;
+    }
+
+    room.gameRunning = true;
+    connection.socket.emit("game started", true);
+    startGameLoop(roomId);
+}
+
+function joinRoom(connection, roomId) {
+    const room = rooms[roomId];
+
+    if (!roomExists(connection, room)) {
+        return;
+    }
+
+    addPlayerToRoom(room, connection);
+
+    connection.socket.join(roomId);
+
+    let canGuess = checkIfGuessAllowed(room, connection.userId)
+
+    connection.socket.emit("room joined", {
+        room: roomId,
+        rounds: room.rounds,
+        duration: room.duration,
+        roomName: room.roomName ?? "WHOMEGALUL",
+        admin: (connection.userId === room.admin),
+        status: room.gameRunning,
+        currentRound: room.currentRound,
+        currentSeconds: room.currentSeconds,
+        canGuess
+    })
+
+    room.playerCount++;
+}
+
+function roomExists(connection, room) {
+    if (room == undefined) {
+        connection.socket.emit("redirect", "/")
+        return false;
+    }
+    return true;
+}
+
+function addPlayerToRoom(room, connection) {
+    if (room.players[connection.userId] == undefined) {
+        room.players[connection.userId] = {}
+    }
+
+    connection.room = room.room;
+}
+
+function processGuess(connection, guess) {
+    const room = rooms[guess.room];
+
+    if (!checkIfGuessAllowed(room, connection.userId)) {
+        return;
+    }
+
+    submitGuess(room, connection.userId, guess);
+}
+
+function disconnectUser(connection) {
+    if (connection && connection.room) {
+        rooms[connection.room].playerCount--;
+    }
+    delete connection;
 }
